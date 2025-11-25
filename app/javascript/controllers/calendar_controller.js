e81ce1bbcdd0c29112f3bf63a0d147be89dedc74
chiaki555 <chiaki.study555@gmail.com>
2025-11-25 09:37:03 +0900

    create：カレンダーに行事名(固定)
    add：モーダルの紐づけ

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -1,31 +1,59 @@
 import { Controller } from "@hotwired/stimulus"
-import { Calendar } from "@fullcalendar/core" // カレンダーを使う準備
+import { Calendar } from "@fullcalendar/core"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import * as bootstrap from "bootstrap"
 
+// import "@fullcalendar/core/index.css"
+// import "@fullcalendar/daygrid/index.css"
+
 export default class extends Controller {
-  connect() {  // ページにHTML側のdata-controller="calendar"がついた要素が表示されたタイミングで呼ばれる
-    const calendar = new Calendar(this.element, {  // this.element：HTML側のdata-controller="calendar"がついた要素
+  connect() {
+    console.log("Stimulus: calendar controller connected!")
+
+    // 既存カレンダーがあれば破棄
+    if (this.calendar) {
+      this.calendar.destroy()
+      this.calendar = null
+    }
+
+    // DOM をクリア
+    this.element.innerHTML = ""
+
+    // FullCalendar インスタンス作成
+    this.calendar = new Calendar(this.element, {
       plugins: [dayGridPlugin],
       initialView: "dayGridMonth",
+      timeZone: "local",
       events: [
         {
           title: "柚子湯",
-          start: "2025-12-22",
+          start: "2025-12-22",  // 日付変更
           extendedProps: { modalId: "seasonalBathModal" },
+        },
+        {
+          title: "年越しの大祓",
+          start: "2025-12-31",
+          extendedProps: { modalId: "eventModal" },
         }
       ],
-      eventClick: function(info) {
-        console.log("クリックイベント:", info.event.title)
-        console.log("modalId:", info.event.extendedProps.modalId)
+      eventClick: (info) => {
         const modal = document.getElementById(info.event.extendedProps.modalId)
-        console.log("modal要素:", modal)
-        if(modal) {
-          const bootstrapModal = new bootstrap.Modal(modal)
-          bootstrapModal.show()
-        }
+        if (modal) new bootstrap.Modal(modal).show()
       }
     })
-    calendar.render()  // 画面にカレンダーを出す
+
+    this.calendar.render()
+  }
+
+  // 外部からイベント一覧を取得するメソッド
+  getEvents() {
+    if (!this.calendar) return []
+    return this.calendar.getEvents()
+  }
+
+  // 外部から日付を変更するメソッド
+  goToDate(dateStr) {
+    if (!this.calendar) return
+    this.calendar.gotoDate(dateStr)
   }
 }
