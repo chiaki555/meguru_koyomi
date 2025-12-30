98c7de1e9c13d02600e5c6a5e8896b8393d48e2f
chiaki555 <chiaki.study555@gmail.com>
2025-12-30 10:33:59 +0900

    add：行事関連

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,25 @@
 class HomesController < ApplicationController
   def index
-    # DBからSeasonalBathを取得（テンプレート込み）
-    seasonal_bath_records = SeasonalBaths::SeasonalBath.includes(:seasonal_bath_template)
+    # DBから SeasonalBath を取得（テンプレート込み）
+    seasonal_bath_records = ::SeasonalBaths::SeasonalBath.includes(:seasonal_bath_template)
 
     # ファーストクラスコレクションを使う
-    seasonal_baths = SeasonalBaths::SeasonalBaths.new(seasonal_bath_records)
+    seasonal_baths = ::SeasonalBaths::SeasonalBaths.new(seasonal_bath_records)
 
     # カレンダー用データを取得
     calendar_baths = seasonal_baths.calendar_baths
 
+    # DBから Event を取得（テンプレート込み）
+    event_records = ::Events::Event.includes(:event_template)
+
+    # ファーストクラスコレクションを使う
+    events = ::Events::Events.new(event_records)
+
+    # カレンダー用データを取得
+    calendar_events = events.calendar_events
+
     # 後で行事を追加してまとめるため
-    @calendar_items = calendar_baths
+    @calendar_items = calendar_baths + calendar_events
 
     respond_to do |format|
       format.html
